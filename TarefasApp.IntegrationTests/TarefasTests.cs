b5d7f2417b9f1ec312b2c9041145035a0aac7ab2
Igordrs <igordrs@outlook.com>
2024-02-04 11:16:41 -0300

    first commit

---
+++ TarefasApp.IntegrationTests/TarefasTests.cs
@@ -0,0 +1,30 @@
+namespace TarefasApp.IntegrationTests
+{
+    // Testes de integração do ENDPOINT da API de tarefas
+    public class TarefasTests
+    {
+        [Fact(Skip = "Não implementado.")]
+        public async Task PostTarefas_ReturnsSuccessStatusCode()
+        {
+            await Task.CompletedTask;
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public async Task PutTarefas_ReturnsSuccessStatusCode()
+        {
+            await Task.CompletedTask;
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public async Task DeleteTarefas_ReturnsSuccessStatusCode()
+        {
+            await Task.CompletedTask;
+        }
+
+        [Fact(Skip = "Não implementado.")]
+        public async Task GetTarefas_ReturnsSuccessStatusCode()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
